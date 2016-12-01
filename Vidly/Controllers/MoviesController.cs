cafc0406f53ecf7f192d8c32e3d9e76a78190b9d
anphonic <abilly@gmail.com>
2016-11-30 22:07:42 -0600

    Section2 Lecture 11

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor.Generator;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -16,7 +17,28 @@ namespace Vidly.Controllers
             {
                 Name = "Shrek!"
             };
-            return View(movie);
+           // return View(movie);
+
+            //return Content("Hellow World");
+            //return HttpNotFound();
+
+            return RedirectToAction("Index","Home", new {page = 1, sorttBy="name"});
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+id);
+        }
+        //optional params  ? makes int nullable. string is be default
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
         }
     }
 }
